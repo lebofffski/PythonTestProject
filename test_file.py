653502488321ae15ca2b58ef8130a61baa6d243f
lebofffski <83531747+lebofffski@users.noreply.github.com>
2023-03-11 00:18:15 +0400

    Delete test_file.py

--- test_file.py
+++
@@ -1,8 +0,0 @@
-# import pandas as pd
-# import numpy as np
-from main import summarize_dataframe, np, pd
-#Example testing
-df = pd.read_csv('test_dataset.csv', sep = ',', names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
-
-if __name__ == '__main__':
-    print(summarize_dataframe(df, input('Choose output type (markdown, html or xlsx):')))
